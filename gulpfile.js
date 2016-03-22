9789d7ab3a3ba073f5cb1085e79a69c89cb0dff5
dkfiresky <dkholodilin@live.com>
2016-03-22 11:03:11 +0200

    FIX: gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,54 @@
 const gulp = require( 'gulp' ),
   minify = require( 'gulp-minify' ),
   header = require( 'gulp-header' ),
+  rename = require( 'gulp-rename' ),
   fs = require( 'fs' ),
   path = require( 'path' ),
-  pkg = JSON.parse( fs.readFileSync( './package.json' ) ),
-  DIST = path.join(
-    process.env.WWW_ROOT,
-    'wordpress.local',
-    'www',
-    'wp-content',
-    'themes',
-    'blank',
-    'assets',
-    'js'
-  );
+  pkg = JSON.parse( fs.readFileSync( './package.json' ) );
 
-gulp.task( 'dist', ( done ) => {
+const DIST = path.join( process.env.WWW_ROOT,
+'wordpress.local', 'www', 'wp-content', 'themes',
+'blank', 'assets', 'js' );
 
-  let filepath = path.join( DIST, 'super-guacamole.js' );
-
-  fs.unlink( filepath, ( error ) => {
-    gulp.src( './src/*.js' )
-      .pipe( minify( {
-        mangle: true
-      } ) )
-      .pipe( header( `/**\n * ${ pkg.name } - ${ pkg.description }\n * @version v${ pkg.version }\n * @link ${ pkg.homepage }\n * @license ${ pkg.license }\n*/\n` ) )
-      .pipe( gulp.dest( './dist/' ) );
-
+function unlink( filepath ) {
+  return ( done ) => {
+    try {
+      fs.unlinkSync( filepath );
+    } catch( error ) {
+      // swallow the error
+    }
     done();
-  } );
+  };
+};
+
+function unlinkDir( dirpath ) {
+  return ( done ) => {
+    try {
+      fs.rmdirSync( filepath );
+    } catch( error ) {
+      // swallow the error
+    }
+    done();
+  };
+};
+
+gulp.task( 'unlink.published', unlink( path.join( DIST, 'super-guacamole.js' ) ) );
+gulp.task( 'unlink.dist', unlinkDir( './dist/' ) );
+gulp.task( 'unlink', [ 'unlink.published', 'unlink.dist' ] );
+
+gulp.task( 'dist', [ 'unlink' ], () => {
+  gulp.src( './src/*.js' )
+    .pipe( minify( {
+      mangle: true
+    } ) )
+    .pipe( header( `/**\n * ${ pkg.name } - ${ pkg.description }\n * @version v${ pkg.version }\n * @link ${ pkg.homepage }\n * @license ${ pkg.license }\n*/\n` ) )
+    .pipe( rename( ( filepath ) => {
+      if ( filepath.basename.indexOf( '-min' ) > -1 ) {
+        filepath.basename = filepath.basename.replace( /\-min/, '' );
+        filepath.extname = '.min.js';
+      }
+    } ) )
+    .pipe( gulp.dest( './dist/' ) );
 } );
 
 gulp.task( 'default', [ 'dist' ], () => {
