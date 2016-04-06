a1da4f634c1b57e4183833e9affca138acb2e93e
dkfiresky <dkholodilin@live.com>
2016-04-06 16:49:07 +0300

    UPD

--- src/super-guacamole.js
+++ src/super-guacamole.js
@@ -329,7 +329,6 @@
       }
 
       width += $attachedNode.data( 'width' );
-      width += ( $attachedNode.data( 'width' ) - $attachedNode.width() );
 
       if ( width > maxWidth && index >= self.options.min_children ) {
         $attachedNode.addClass( 'super-guacamole__menu__hidden' );
